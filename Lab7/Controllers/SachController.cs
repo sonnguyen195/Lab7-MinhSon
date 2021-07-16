f3039b300313c5e1d8899cae1ac4d764f0a45f78
sonnguyen195 <sonson195.sn@gmail.com>
2021-07-16 11:35:34 +0700

    lab 7

---
+++ Lab7/Controllers/SachController.cs
@@ -0,0 +1,32 @@
+﻿using Lab7.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lab7.Controllers
+{
+    public class SachController : ApiController
+    {
+        QuanLiSach[] saches = new QuanLiSach[]
+        {
+            new QuanLiSach{Id = 1, Title="abcxyzzzz",AuthorName="Nguyen Minh Son",Price=1,Content="shfagfugfuygafyugasuydfgauysdg"},
+            new QuanLiSach{Id = 2, Title="abcxyzzzz",AuthorName="Nguyen Minh Son",Price=1,Content="shfagfugfuygafyugasuydfgauysdg"},
+        };
+        public IEnumerable<QuanLiSach> GetAll()
+        {
+            return saches;
+        }
+        public IHttpActionResult GetSach(int Id)
+        {
+            var sach = saches.FirstOrDefault((p) => p.Id == Id);
+            if(sach == null)
+            {
+                return NotFound();
+            }
+            return Ok(sach);
+        }
+    }
+}
